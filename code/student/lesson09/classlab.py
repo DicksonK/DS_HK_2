4c69729715fe27705240db20347cad4ea440876c
DicksonK <dickson809@gmail.com>
2014-05-26 22:05:54 +0800

    What's up

--- code/student/lesson09/classlab.py
+++ code/student/lesson09/classlab.py
@@ -35,6 +35,7 @@ gnb = GaussianNB()
 y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
 
 print("Number of mislabeled points : %d" % (iris.target != y_pred).sum())
+print "\n\n"
 
 """
 Problem: Classify text as either an insult or not an insult
@@ -64,9 +65,11 @@ test = pd.read_csv(DATA_DIR + 'insults/test-utf8.csv')
 # ngram range: relatively useful, how many words to consider per feature.
 
 ########## TRANSFORM THE DATA: COUNT VECTORIZE, CLEANING
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(ngram_range=(1,1), lowercase=True)
 X_train = vectorizer.fit_transform(train.Comment)
 X_test = vectorizer.transform(test.Comment)
+
+
 
 ##### USE THE WORD COUNT MATRIX TO PREDICT INSULT/NOT INSULT (1/0)
 ### BUILD A TRAINING AND TEST SET
@@ -75,7 +78,7 @@ model = naive_bayes.MultinomialNB().fit(X_train, list(train.Insult))
 ###### TEST RESULTS
 ###### CROSS VALIDATE
 ####### DISPLAY RESULTS: AUC TO CHECK FOR ERROR
-print cross_validation.cross_val_score(naive_bayes.MultinomialNB(), X_train, train.Insult)
+print cross_validation.cross_val_score(naive_bayes.MultinomialNB(), X_train, train.Insult, cv=10).mean()
 fpr, tpr, thresholds = metrics.roc_curve(train.Insult, model.predict(X_train), pos_label=1)
 print metrics.auc(fpr, tpr)
 
@@ -88,6 +91,15 @@ submission = pd.DataFrame({'id' : test.id, 'insult': predictions})
 
 submission.to_csv(os.getcwd()+'/submission.csv', index=False)
 
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vect = TfidfVectorizer(sublinear_tf=True, max_df=0.7)
+X_train = vect.fit_transform(train.Comment)
+X_test = vect.transform(test.Comment)
+model = naive_bayes.MultinomialNB().fit(X_train, list(train.Insult))
+print cross_validation.cross_val_score(naive_bayes.MultinomialNB(), X_train, train.Insult, cv=10).mean()
+fpr, tpr, thresholds = metrics.roc_curve(train.Insult, model.predict(X_train), pos_label=1)
+print metrics.auc(fpr, tpr)
 
 '''
 print train.describe()
