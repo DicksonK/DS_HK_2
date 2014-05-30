94b7062d939a0379d517794c8ea9589485705740
DicksonK <dickson809@gmail.com>
2014-05-30 13:54:27 +0800

    use feature selection

--- code/student/ongoing/baseball/attemp_1.py
+++ code/student/ongoing/baseball/attemp_1.py
@@ -8,7 +8,7 @@
 # Date: 2014-05-19
 
 import pandas as pd
-from sklearn import linear_model, metrics
+from sklearn import linear_model, metrics, feature_selection
 
 DATA_DIR = '../../../../data/baseball/'
 
@@ -127,4 +127,64 @@ print 'MSE:',metrics.mean_squared_error(lm2.predict(train_X), train_y)
 print '\n'
 
 
+#============== 
+
+train_X = b2011[['weight', 'height', 'HR']].values
+train_y = b2011['salary'].values
 #
+test_X = b2012[['weight', 'height', 'HR']].values
+b2012_csv = b2012[['playerID','yearID', 'salary']]
+
+
+try_model = linear_model.Ridge(alpha=0.5)
+try_model.fit(train_X, train_y)
+#
+#
+# Checking performance, roughly .19
+print 'R-Squared:',try_model.score(train_X, train_y)
+# Checking MSE, roughly terrible
+print 'MSE:',metrics.mean_squared_error(try_model.predict(train_X), train_y)
+#
+print '\n'
+
+b2011_num = b2011._get_numeric_data()
+b2011_num = b2011_num.dropna(axis=1)
+salary = b2011_num['salary']
+b2011_num = b2011_num.drop(['salary'],1)
+
+fp_value = feature_selection.univariate_selection.f_regression(b2011_num, salary)
+p_value = zip(b2011_num.columns.values,fp_value[1])
+print sorted(p_value,key=lambda x: x[1])
+
+'''
+[('birthYear', 7.9835736658094558e-24), ('lahmanID', 1.4981569016438451e-19), ('RBI', 2.3423696853156987e-19), 
+('HR', 2.1781665697039328e-18), ('HR*3', 2.1781665697043661e-18), ('BB', 1.6894807429368926e-16), ('IBB', 1.3518139523605894e-15), 
+('GIDP', 3.3671114818920081e-15), ('R', 5.5242446285898393e-15), ('R*3', 5.524244628591959e-15), ('H', 3.8926959093146868e-14), 
+('AB', 1.4713547901030828e-13), ('X2B', 2.0126968058913591e-13), ('SF', 1.5442454315422193e-12), ('SO', 4.5120325955358599e-09), 
+('weight', 1.4742817482563304e-07), ('G', 2.7770222456756615e-07), ('G_batting', 2.7770222456756615e-07), ('G_old', 2.7770222456756615e-07), 
+('HBP', 4.3295685791807793e-06), ('height', 0.024490536292798772), ('birthDay', 0.025050282411500129), ('SB', 0.056525713590704518), 
+('USA', 0.15687171768959324), ('CS', 0.16214722686250782), ('X3B', 0.1758587098104967), ('birthMonth', 0.22107508528233263), 
+('yearID', nan), ('stint', nan), ('CAN', 0.32990232740163583), ('D.R.', 0.36608449516973751), ('P.R.', 0.40215980142108221), 
+('SH', 0.40730098572339368), ('Australia', 0.6039726888491268), ('Venezuela', 0.81469953950938012), ('Germany', 0.88649615124230063)]
+'''
+
+train_X = b2011[['Germany', 'Venezuela', 'Australia', 'SH', 'P.R.', 'D.R.', 'CAN', 'birthMonth', 'X3B', 'CS', 'USA', 'SB', 
+	'birthDay', 'height', 'HBP']].values
+train_y = b2011['salary'].values
+#
+test_X = b2012[['Germany', 'Venezuela', 'Australia', 'SH', 'P.R.', 'D.R.', 'CAN', 'birthMonth', 'X3B', 'CS', 'USA', 'SB', 
+	'birthDay', 'height', 'HBP']].values
+b2012_csv = b2012[['playerID','yearID', 'salary']]
+
+
+try_model = linear_model.Ridge()
+try_model.fit(train_X, train_y)
+#
+#
+# Checking performance, roughly .19
+print 'R-Squared:',try_model.score(train_X, train_y)
+# Checking MSE, roughly terrible
+print 'MSE:',metrics.mean_squared_error(try_model.predict(train_X), train_y)
+#
+print '\n'
+
